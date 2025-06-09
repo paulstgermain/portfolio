a9eb3345626222ed256dc8fdff485f658c4fb775
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-09 01:08:11 -0400

    shifts fonts to tailwind theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${bungee.variable} ${istokWeb.variable} antialiased`}
+        className={`antialiased`}
       >
         <LayoutWrapper>
           {children}
