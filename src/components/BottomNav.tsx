6558716adcd82da860833fd217ea9d044757d560
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-13 01:30:57 -0400

    expands dynamic theme logic to include BottomNav component

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,7 +1,11 @@
 
-export default function BottomNav() {
+interface BottomNavProps {
+  bgClass: string;
+}
+
+export default function BottomNav({bgClass}: BottomNavProps) {
   return (
-    <nav className="w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 bg-background rounded-[40px] mx-auto pl-12 pr-12 absolute bottom-4 left-1/2 transform -translate-x-1/2">
+    <nav className={`w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 ${bgClass} rounded-[40px] mx-auto pl-12 pr-12 absolute bottom-4 left-1/2 transform -translate-x-1/2`}>
     </nav>
   );
 }
