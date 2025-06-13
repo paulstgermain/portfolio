a0a921339d906a2265ac08eaa95b416c9c16c008
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-13 02:08:51 -0400

    adds color transition to top and bottom navbars

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -13,7 +13,7 @@ const navItems = [
 
 export default function BottomNav({bgClass}: BottomNavProps) {
   return (
-    <nav className={`w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 ${bgClass} rounded-[40px] mx-auto pl-12 pr-12 absolute bottom-4 left-1/2 transform -translate-x-1/2`}>
+    <nav className={`w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 ${bgClass} rounded-[40px] mx-auto pl-12 pr-12 absolute bottom-4 left-1/2 transform -translate-x-1/2 transition-colors duration-300 ease-in-out`}>
       <div className="flex items-center space-evenly h-full w-2/5 justify-between">
           {navItems.map(({ href, src, alt }) => {
             return (
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ interface NavBarProps {
 }
 export default function NavBar({bgClass, altBgClass}: NavBarProps) {
   return (
-    <div className={`w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 ${bgClass} rounded-[40px] mx-auto pl-12 pr-12 relative top-4 flex items-center justify-between shadow-[8px_10px_0_0] ${altBgClass}`}>
+    <div className={`w-15/16 h-11 sm:h-16 md:h-20 lg:h-22 ${bgClass} rounded-[40px] mx-auto pl-12 pr-12 relative top-4 flex items-center justify-between shadow-[8px_10px_0_0] ${altBgClass} transition-colors duration-300 ease-in-out`}>
       <div className="flex-shrink-0">
       <Image
         src="/svgs/music_note.svg"
