37131969ea270d0b9f2828baec310b8cef97a74b
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-12 22:41:18 -0400

    adds basic NavBar elements, styling, positioning

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
+
 export default function NavBar() {
   return (
-    <nav>
+    <nav className="w-15/16 h-22 bg-homenav rounded-[40px] mx-auto pl-12 pr-12 relative top-4 flex items-center justify-between shadow-[8px_10px_0_0_#9d3256]">
       <Image
         height={37}
         width={38}
         src="/svgs/music_note.svg"
         alt="Music Note Icon"
       />
+      <p className="text-primary text-4xl font-bungee">Paul St.Germain</p>
     </nav>
   )
 }
