09ed118a3884abece37a138850d52375b38e349e
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-18 16:44:03 -0400

    adds SVGs, aligns all items per design spec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,36 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <main className="flex">
-        <div>
+      <main className="relative w-screen flex justify-center">
+        <div className="relative w-90 h-140 mt-20">
+          <div className="absolute inset-0 z-10 rounded-[40px] overflow-hidden">
+            <Image
+              src="/imgs/water.webp"
+              alt="Centered image of water"
+              layout="fill"
+              objectFit="cover"
+              className={"rounded-[inherit]"}
+            />
+          </div>
 
-        </div>
-        <div className="w-90 h-140 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-[40px] overflow-hidden">
-          <Image
-          src="/imgs/water.webp"
-          alt="Centered image of water"
-          layout="fill"
-          objectFit="cover"
-          className={"rounded-[inherit]"}
-          />
-        </div>
-        <div>
+          <div className="absolute left-[-370px] -top-8">
+            <Image
+              src="svgs/landing-left.svg"
+              alt=""
+              width="375"
+              height="375"
+            />
+          </div>
 
+          <div className="absolute right-[-370px] -top-8">
+            <Image
+              src="svgs/landing-right.svg"
+              alt=""
+              width="375"
+              height="375"
+            />
+          </div>
         </div>
       </main>
-    </>
   );
 }
-
-/* 
-  1. Import Image component
-  2. Set up Div layout (Left Div, Right Div, each contains an SVG - svgs/landing-left.svg, svgs/landing-right.svg)
-  3. Add Image component, position absolute, centered on screen, src = imgs/water.webp (can adjust left/right divs to line SVG up from there)
-*/
