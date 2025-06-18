cb222ad83566ae225920c2f804d0e3040916356f
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-18 16:44:37 -0400

    updating Tailwind classes for mobile responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
       <main className="relative w-screen flex justify-center">
-        <div className="relative w-90 h-140 mt-20">
+        <div
+          className="
+            relative
+            lg:w-90       lg:h-[560px]       lg:mt-20
+            md:w-72    md:h-[480px]    md:mt-16
+            sm:w-64    sm:h-[416px]    sm:mt-12
+            xs:w-52    xs:h-[384px]    xs:mt-10
+          "
+        >
           <div className="absolute inset-0 z-10 rounded-[40px] overflow-hidden">
             <Image
               src="/imgs/water.webp"
@@ -14,7 +22,20 @@ export default function Home() {
             />
           </div>
 
-          <div className="absolute left-[-370px] -top-8">
+          <div
+            className="
+              absolute
+              -top-8
+              lg:-left-92
+              md:-left-80
+              sm:-left-72
+              xs:-left-64
+              lg:w-[375px]
+              md:w-[325px]
+              sm:w-[280px]
+              xs:w-[240px]
+            "
+          >
             <Image
               src="svgs/landing-left.svg"
               alt=""
@@ -23,7 +44,20 @@ export default function Home() {
             />
           </div>
 
-          <div className="absolute right-[-370px] -top-8">
+          <div
+            className="
+              absolute
+              -top-8
+              lg:-right-92
+              md:-right-80
+              sm:-right-72
+              xs:-right-64
+              lg:w-[375px]
+              md:w-[325px]
+              sm:w-[280px]
+              xs:w-[240px]
+            "
+          >
             <Image
               src="svgs/landing-right.svg"
               alt=""
