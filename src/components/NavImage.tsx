b43105ec9df64a17dbfbaa19eae7fa1034c25504
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-17 21:43:38 -0400

    adds overlay text to NavImage component

--- src/components/NavImage.tsx
+++ src/components/NavImage.tsx
@@ -10,29 +10,33 @@ const imageSettingsByPathname: Record<string, any> = {
   },
   "/about": {
     src: "/imgs/trees.png",
-    alt: "About Image",
+    alt: "About page image",
     className: "w-135 h-90 [clip-path:polygon(46%_0%,100%_0%,100%_100%,-20%_100%)]",
+    text: "About",
     position: "absolute bottom-11 right-12",
     shadow: "shadow-aboutnav-alt shadow-[8px_10px_0_0]"
   },
   "/projects": {
     src: "/imgs/buildings.png",
-    alt: "Projects Image",
+    alt: "Projects page image",
     className: "w-75 h-165",
+    text: "Projects",
     position: "absolute bottom-11 right-12",
     shadow: "shadow-projectsnav-alt shadow-[8px_10px_0_0]"
   },
   "/blog": {
     src: "/imgs/paper.png",
-    alt: "Blog Image",
+    alt: "Blog page image",
     className: "w-150 h-45",
+    text: "Blog",
     position: "absolute bottom-11 right-12",
     shadow: "shadow-blognav-alt shadow-[8px_10px_0_0]"
   },
   "/contact": {
     src: "/imgs/bust.png",
-    alt: "Contact Image",
+    alt: "Contact page image",
     className: "w-130 h-90",
+    text: "Contact",
     position: "absolute bottom-25 left-12",
     shadow: "shadow-contactnav-alt shadow-[-8px_10px_0_0]"
   },
@@ -41,7 +45,7 @@ const imageSettingsByPathname: Record<string, any> = {
 export default function NavImage({ pathname }: { pathname: string }) {
   const config = imageSettingsByPathname[pathname];
   if (!config) return null;
-  const { src, alt, className, position, shadow } = config;
+  const { src, alt, className, text, position, shadow } = config;
 
   return (
     <>
@@ -56,6 +60,7 @@ export default function NavImage({ pathname }: { pathname: string }) {
               className={"rounded-[inherit]"}
             />
           }
+          <div className="absolute bottom-8 right-12 text-primary font-bungee text-4xl select-none">{text}</div>
         </div>
       </div>
     </>
