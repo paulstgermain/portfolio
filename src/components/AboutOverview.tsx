714fa6b79149d042cb507e91eb6e86c78ef082e5
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-24 02:08:44 -0400

    fixes for deploy

--- src/components/AboutOverview.tsx
+++ src/components/AboutOverview.tsx
@@ -81,22 +81,22 @@ export default function AboutOverview() {
             scrollbar-about
           ">
             <p className={`${overviewText}`}>
-              Hi, I'm Paul! I'm a Software Engineer with 3 years of
+              Hi, I&apos;m Paul! I&apos;m a Software Engineer with 3 years of
               focused experience using technology to build products
               and solve problems for nonprofits and startups in the 
               USA and the UK.
             </p>
             <p className={`${overviewText}`}>
-              As a 90's kid, the internet has always been my favorite place—my playground, library, and space for creative expression.
+              As a 90&apos;s kid, the internet has always been my favorite place—my playground, library, and space for creative expression.
             </p>
             <p className={`${overviewText}`}>
               That lifelong love evolved into a passion for web development, where I bring energy, curiosity, and a mission-driven mindset to every project. 
             </p>
             <p className={`${overviewText}`}>
-              I thrive when I'm using code to create positive change or inject some creativity and joy back into the online experience.
+              I thrive when I&apos;m using code to create positive change or inject some creativity and joy back into the online experience.
             </p>
             <p className={`${overviewText}`}>
-              When I’m not coding, I enjoy digging into internet nostalgia, taking in nature through the lens of my camera, coexisting with my cats, and reading.
+              When I&apos;m not coding, I enjoy digging into internet nostalgia, taking in nature through the lens of my camera, coexisting with my cats, and reading.
             </p>
             <p className={`${overviewText}`}>
               Here are some of things I currently like to build with:
