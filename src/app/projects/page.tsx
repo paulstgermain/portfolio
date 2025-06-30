e3f4dc45476ca96bd6768c44306fec251e5eeaa5
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-30 00:58:55 -0400

    adjust columns for projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
 export default function Projects() {
   return (
-    <main>
-      <div className="flex flex-col h-screen">
-        <div>
+    <main className="flex-1 flex flex-col">
+      <div className="flex flex-row h-full">
+        <div className="flex-1 border-blue-500 border-2 ml-14">
           {/* Projects images go here */}
         </div>
-        <div>
+        <div className="flex-3 border-projectsnav border-2 mr-100">
           {/* Projects content goes here, shown to user when they click on a project image */}
         </div>
       </div>
