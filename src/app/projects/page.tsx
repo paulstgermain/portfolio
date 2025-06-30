5ac8b5853bf833169c0161422df44ad5e6d01387
Paul St.Germain <paulg.stgermain@gmail.com>
2025-06-30 03:02:58 -0400

    adds state and handleClick for project images

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,19 +1,41 @@
+"use client"
+import { SetStateAction, useState } from 'react';
 import Image from 'next/image';
 import data from '../../data/projects.json';
 
-export default async function Projects() {
+interface Project {
+  id: string,
+  title: string,
+  techStack: string[],
+  description: string,
+  shortBlurb: string,
+  links: {
+    website: string | null,
+    github: {
+      text: string,
+      link: string
+    }[]
+  },
+  imageSrc: string,
+  imageAlt: string
+}
+
+export default function Projects() {
+  const [activeProject, setActiveProject] = useState<Project | null>(null);
   const projects = data.projects;
 
-  console.log(projects);
+  const handleClick = (project: Project) => {
+    setActiveProject(project);
+  }
+
   return (
     <main className="h-full flex flex-col">
       <div className="flex flex-row h-full">
         <div className="flex-6  ml-14 flex flex-col">
           <div className="h-full scrollbar-styles scrollbar-projects overflow-y-scroll">
             <div className="flex flex-col py-10 space-y-10">
-              {/* Projects images go here */}
               {projects && projects.map((project, index) => (
-                <div key={project.title || index} className="rounded-[40px] overflow-hidden w-89 h-53 mx-auto flex-shrink-0">
+                <div key={project.title || index} className="rounded-[40px] overflow-hidden w-89 h-53 mx-auto flex-shrink-0 cursor-pointer" onClick={() => handleClick(project)}>
                   <Image src={project.imageSrc} alt={project.imageAlt} width={386} height={217} />
                 </div>
               ))}
