e9fd0b42e959ec1c9c8c65e18b35574d580c46b2
Paul St.Germain <paulg.stgermain@gmail.com>
2025-09-07 23:53:58 -0400

    adjust project screenshot column border implementation, prepping for responsiveness

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -30,47 +30,49 @@ export default function Projects() {
   }
 
   return (
-    <main className="h-full flex flex-col">
+    <main className="h-full">
       <div className="flex flex-row h-full">
         <div className="flex-6  ml-14 flex flex-col">
           <div className="h-full scrollbar-styles scrollbar-projects overflow-y-scroll">
-            <div className="flex flex-col py-10 space-y-10">
+            <div className="flex flex-col py-10 space-y-10 ">
               {projects && projects.map((project, index) => (
-                <div key={project.id || index} className="relative">
-                  <Image src="/svgs/project-border.svg" alt="Project screenshot border" width={400} height={300} className={`${activeProject === project ? 'opacity-100' : 'opacity-0'} transition-all duration-250 ease-in-out`} />
-                  <div className={`absolute inset-0 mt-7 ml-6 rounded-[40px] overflow-hidden w-89 h-53 mx-auto flex-shrink-0 cursor-pointer`} onClick={() => handleClick(project)}>
-                    <Image src={project.imageSrc} alt={project.imageAlt} width={386} height={217} />
+                  <div key={project.id || index} className={`relative rounded-[40px] overflow-hidden w-89 h-53 mx-auto cursor-pointer ${activeProject && activeProject.id === project.id ? "projects-frame" : ""} transition-all duration-300 ease-in-out`} onClick={() => handleClick(project)}>
+                    <Image src={project.imageSrc} alt={project.imageAlt} width={356} height={212} />
                   </div>
-                </div>
               ))}
             </div>
           </div>
         </div>
-        <div className="text-primary font-istok-web flex-7 mr-95 scrollbar-styles scrollbar-projects overflow-y-auto">
+        <div className={`text-primary font-istok-web flex-7 mr-95 scrollbar-styles scrollbar-projects overflow-y-auto ${activeProject ? "projects-content-frame" : ""}`}>
           {/* Projects content goes here, shown to user when they click on a project image */}
           {activeProject && 
           <>
             <div className="text-2xl mt-10 mx-2">
-              <p className="border-projectsnav border-2 mb-2 px-2 w-fit">{activeProject.title} | {activeProject.year}</p>
-              <p className="border-projectsnav border-2 text-xl mb-2 px-2">{activeProject.techStack.map(tech => `${tech} `)}</p>
+              <p className=" mb-2 px-2 w-fit">{activeProject.title} | {activeProject.year}</p>
+              <p className=" text-xl mb-2 px-2">{activeProject.techStack.map((tech, i) => <span key={tech + i}>{tech} <span className="text-projectsnav">| </span></span>)}</p>
             </div>
-            <div className="flex flex-col border-projectsnav border-2 mx-2 px-2">
+            <div className="flex flex-col  mx-2 px-2">
               <p className="text-xl">{activeProject.description}</p>
               {(activeProject.links.website || activeProject.links.github.length > 0) &&
               <>
               <p className="text-xl underline mt-3">Links:</p>
                 <ul className="mt-3">
                   {activeProject.links.website &&
-                    <li key={activeProject.id}>Website: <a href={activeProject.links.website} target="_blank" className="hover:underline cursor-pointer">{activeProject.links.website}</a></li>
+                    <li key={activeProject.links.website}>Website: <a href={activeProject.links.website} target="_blank" className="hover:underline cursor-pointer">{activeProject.links.website}</a></li>
                   }
                   {activeProject.links.github.length > 0 &&
-                    activeProject.links.github.map(url => <li key={activeProject.id}><a href={url.link} target="_blank" className="hover:underline cursor-pointer">{url.text}</a></li>)
+                    activeProject.links.github.map(url => <li key={url.link}><a href={url.link} target="_blank" className="hover:underline cursor-pointer">{url.text}</a></li>)
                   }
                 </ul>
               </>
               }
             </div>
           </>
+          }
+          {!activeProject &&
+          <div className="text-2xl mt-10 mx-2">
+            <p className="mb-2 px-2 w-fit">Select a project to view its details</p>
+          </div>
           }
         </div>
       </div>
